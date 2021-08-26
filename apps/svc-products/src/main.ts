e5c5cc51de956968d00a2f976b7a7da565c24839
Creotip <creotip@gmail.com>
2021-08-27 00:16:16 +0300

    sha commit tag

--- apps/svc-products/src/main.ts
+++ apps/svc-products/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to svc-products! ARGOCD2222' });
+  res.send({ message: 'Welcome to svc-products! ARGOCD 999' });
 });
 
 const port = process.env.port || 3333;
